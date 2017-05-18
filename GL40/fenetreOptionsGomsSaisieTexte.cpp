cffa6f4752091999c1f114e16e993bf8fece412e
MetaKnight73 <nicolas.hergenroether@utbm.fr>
2017-05-18 13:18:03 +0200

    Add files via upload

---
+++ GL40/fenetreOptionsGomsSaisieTexte.cpp
@@ -0,0 +1,159 @@
+#include "fenetreOptionsGomsSaisieTexte.h"
+#include <iostream>
+
+FenetreOptionsGomsSaisieTexte::FenetreOptionsGomsSaisieTexte(QWidget *parent) : QDialog(parent) {
+
+    // On assigne une taille fixe de fenêtre à 400x200
+    setFixedSize(400,200);
+
+    // Layouts de la fenêtre
+    layout = new QGridLayout;
+    layout->setHorizontalSpacing(5);
+    layout->setColumnMinimumWidth(0, 15);
+    layout->setColumnMinimumWidth(1, 15);
+    layout->setColumnMinimumWidth(2, 15);
+    layout->setColumnMinimumWidth(3, 15);
+    layoutUsers = new QHBoxLayout;
+    layoutValiderAnnuler = new QHBoxLayout;
+
+    // Labels de la fenêtre
+    label1 = new QLabel;
+    label2 = new QLabel;
+    labelMots = new QLabel;
+
+    // Spinners
+    spinParam1 = new QDoubleSpinBox;
+    spinParam2 = new QDoubleSpinBox;
+    spinNbMots = new QSpinBox;
+    spinNbMots->setAlignment(Qt::AlignCenter);
+    spinParam1->setFixedWidth(110);
+    spinParam1->setAlignment(Qt::AlignCenter);
+    spinParam2->setFixedWidth(110);
+    spinParam2->setAlignment(Qt::AlignCenter);
+
+    // Assignation des textes des labels
+    label1->setText("Nombre de caractères maximal d'un mot : ");
+    label2->setText("Temps d'activité mentale (M) : ");
+    labelMots->setText("Sélectionnez le nombre de mots à saisir : ");
+
+    // On gère les valeurs du spinner 1
+    spinParam1->setMinimum(1);
+    spinParam1->setMaximum(20);
+    spinParam1->setSingleStep(1);
+    spinParam1->setValue(10);
+
+    // On gère les valeurs du spinner 2
+    spinParam2->setMinimum(0.01);
+    spinParam2->setMaximum(2);
+    spinParam2->setSingleStep(0.01);
+    spinParam2->setValue(1.35);
+
+    // On gère les valeurs du spinner du nombre de clics
+    spinNbMots->setMinimum(1);
+    spinNbMots->setMaximum(20);
+    spinNbMots->setSingleStep(1);
+    spinNbMots->setValue(10);
+
+    // Textes de boutons
+    start = new QPushButton("Valider");
+    start->setFixedHeight(40);
+    start->setStyleSheet(QString("background-color: #32CD32; border: 1px solid black;"));
+
+    cancel = new QPushButton("Annuler");
+    cancel->setFixedHeight(40);
+    cancel->setStyleSheet(QString("background-color: #DC143C; border: 1px solid black;"));
+
+    userNormal = new QPushButton("Utilisateur normal (par défaut)");
+    userNormal->setFixedHeight(40);
+
+    userExpert = new QPushButton("Utilisateur expert");
+    userExpert->setFixedHeight(40);
+
+    layoutUsers->addWidget(userNormal);
+    layoutUsers->addWidget(userExpert);
+
+    layoutValiderAnnuler->addWidget(start);
+    layoutValiderAnnuler->addWidget(cancel);
+
+    // On ajoute les éléments dans la page
+    layout->addWidget(label1, 0, 0);
+    layout->addWidget(spinParam1, 0, 1);
+    layout->addWidget(label2, 1, 0);
+    layout->addWidget(spinParam2, 1, 1);
+    layout->addWidget(labelMots, 2, 0);
+    layout->addWidget(spinNbMots, 2, 1);
+    layout->addLayout(layoutUsers, 3, 0, 1, 6, 0);
+    layout->addLayout(layoutValiderAnnuler, 4, 0, 1, 6, 0);
+
+    setLayout(layout);
+
+    userNormal->setDown(true);
+    userNormal->setEnabled(false);
+
+    // Les connexions
+    connect(start, SIGNAL(clicked()), this, SLOT(accept()));
+    connect(cancel, SIGNAL(clicked()), this, SLOT(close()));
+    connect(userNormal, SIGNAL(clicked()), this, SLOT(restoreDefaultValues()));
+    connect(userExpert, SIGNAL(clicked()), this, SLOT(restoreExpertValues()));
+    connect(spinParam2, SIGNAL(valueChanged(double)), this, SLOT(onChange()));
+
+}
+
+void FenetreOptionsGomsSaisieTexte::restoreDefaultValues() {
+
+    spinParam1->setValue(10);
+    spinParam2->setValue(1.35);
+    spinNbMots->setValue(10);
+    userExpert->setDown(false);
+    userExpert->setEnabled(true);
+
+}
+
+void FenetreOptionsGomsSaisieTexte::restoreExpertValues() {
+
+    spinParam1->setValue(10);
+    spinParam2->setValue(1.25);
+    spinNbMots->setValue(10);
+    userNormal->setDown(false);
+    userNormal->setEnabled(true);
+
+}
+
+void FenetreOptionsGomsSaisieTexte::onChange() {
+
+    double param2 = spinParam2->value();
+
+    if(param2 == (double)1.35) {
+        userNormal->setDown(true);
+        userNormal->setDisabled(true);
+    }
+    else if(param2 == (double)1.25) {
+        userExpert->setDown(true);
+        userExpert->setDisabled(true);
+    }
+    else {
+        userNormal->setDown(false);
+        userExpert->setDown(false);
+        userNormal->setEnabled(true);
+        userExpert->setEnabled(true);
+    }
+
+}
+
+int FenetreOptionsGomsSaisieTexte::getNbMots() {
+
+    return spinNbMots->value();
+
+}
+
+double FenetreOptionsGomsSaisieTexte::getParametre1() {
+
+    return spinParam1->value();
+
+}
+
+double FenetreOptionsGomsSaisieTexte::getParametre2() {
+
+    return spinParam2->value();
+
+}
---
+++ GL40/fenetrePrincipale.cpp
@@ -0,0 +1,124 @@
+#include "fenetrePrincipale.h"
+#include <QGridLayout>
+#include <QBoxLayout>
+#include <QWidget>
+#include <QLabel>
+
+FenetrePrincipale::FenetrePrincipale(QWidget *parent) : QMainWindow(parent) {
+
+    // Taille fixe à 1280x720, envisager une adaptation
+    setFixedSize(1280, 720);
+
+    // Layout de la page
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->setSpacing(50);
+
+    // Widget central de la page
+    QWidget *middle = new QWidget;
+    middle->setLayout(layout);
+    setCentralWidget(middle);
+
+    // Label du titre
+    QLabel *label = new QLabel("Loi de Fitts et Goms-Keystroke");
+    label->setFont(QFont("Arial Black", 32));
+    label->setAlignment(Qt::AlignCenter);
+    layout->addWidget(label);
+
+    // Label de l'énoncé (première partie)
+    QLabel *label2 = new QLabel("Ceci est un programme permettant d'effectuer des tests afin de comparer les résultats théoriques de plusieurs tests connus tels que la loi de Fitts et Goms-Keystroke avec les valeurs récoltées manuellement lors de ces tests.");
+    label2->setWordWrap(true);
+    label2->setAlignment(Qt::AlignCenter);
+
+    layout->addWidget(label2);
+
+    // Bouton servant à lancer l'application (la partie fitts)
+    btnFitts = new QPushButton("Loi de Fitts");
+    btnFitts->setFixedSize(400, 75);
+    layout->addWidget(btnFitts, 0, Qt::AlignCenter);
+
+    // Bouton servant à lancer l'application (la partie goms 1)
+    btnGoms1 = new QPushButton("Goms-Keystroke (saisie de texte)");
+    btnGoms1->setFixedSize(400, 100);
+    layout->addWidget(btnGoms1, 0, Qt::AlignCenter);
+
+    // Bouton servant à lancer l'application (la partie goms 2)
+    btnGoms2 = new QPushButton("Goms-Keystroke (clics)");
+    btnGoms2->setFixedSize(400, 125);
+    layout->addWidget(btnGoms2, 0, Qt::AlignCenter);
+
+    // Menu bar au sommet
+    menuFile = menuBar()->addMenu("Fichier");
+
+    // Actions de la barre
+    nouveau = new QMenu("Nouveau", this);
+    newFitts = new QAction("Loi de Fitts", nouveau);
+    nouveau->addAction(newFitts);
+    newGoms1 = new QAction("Goms-Keystroke avec saisie de texte", nouveau);
+    nouveau->addAction(newGoms1);
+    newGoms2 = new QAction("Goms-Keystroke avec clics", nouveau);
+    nouveau->addAction(newGoms2);
+
+    about = new QAction("À propos de nous", this);
+    fermer = new QAction("Quitter", this);
+
+    menuFile->addMenu(nouveau);
+    menuFile->insertSeparator(newFitts);
+    menuFile->addAction(about);
+    menuFile->addAction(fermer);
+
+    // On initialise ici la fenêtre des options pour les paramètres de l'application
+    fenetreOptionsFitts = new FenetreOptionsFitts;
+    fenetreOptionsGomsSaisieTexte = new FenetreOptionsGomsSaisieTexte;
+
+    // Les connexions
+    connect(newFitts, SIGNAL(triggered()), this, SLOT(afficheFenetreOptionsFitts()));
+    connect(newGoms1, SIGNAL(triggered()), this, SLOT(afficheFenetreOptionsGomsSaisieTexte()));
+    connect(about, SIGNAL(triggered()), this, SLOT(aPropos()));
+    connect(btnFitts, SIGNAL(clicked()), this, SLOT(afficheFenetreOptionsFitts()));
+    connect(btnGoms1, SIGNAL(clicked()), this, SLOT(afficheFenetreOptionsGomsSaisieTexte()));
+    connect(fermer, SIGNAL(triggered()), this, SLOT(close()));
+
+}
+
+// Méthode appelée pour l'affichage de la fenêtre avec les paramètres de l'application
+void FenetrePrincipale::afficheFenetreOptionsFitts() {
+
+    fenetreOptionsFitts->setModal(true);
+
+    // Si on clique sur valider dans la fenêtre d'options, on va ouvrir la fenêtre avec le test
+    if(fenetreOptionsFitts->exec() == QDialog::Accepted) {
+        setCentralWidget(new FenetreTestFitts(fenetreOptionsFitts->getNbClics(), this, fenetreOptionsFitts->getParametre1(), fenetreOptionsFitts->getParametre2()));
+    }
+
+}
+
+// Méthode appelée pour l'affichage de la fenêtre avec les paramètres de l'application
+void FenetrePrincipale::afficheFenetreOptionsGomsSaisieTexte() {
+
+    fenetreOptionsGomsSaisieTexte->setModal(true);
+
+    // Si on clique sur valider dans la fenêtre d'options, on va ouvrir la fenêtre avec le test
+    if(fenetreOptionsGomsSaisieTexte->exec() == QDialog::Accepted) {
+        setCentralWidget(new FenetreTestGomsSaisieTexte(fenetreOptionsGomsSaisieTexte->getNbMots(), this, fenetreOptionsGomsSaisieTexte->getParametre1(), fenetreOptionsGomsSaisieTexte->getParametre2()));
+    }
+
+}
+
+// Méthode appelée quand quand le test est fini, qui affiche la fenêtre des stats
+void FenetrePrincipale::afficheFenetreStatistiquesFitts(std::vector<StatistiquesFitts> statistiquesFitts) {
+
+    fenetreStatistiquesFitts = new FenetreStatistiquesFitts(statistiquesFitts);
+
+    // Connexion au bouton Recommencer, on réaffiche la fenêtre avec les options
+    connect(fenetreStatistiquesFitts->getBoutonRecommencer(), SIGNAL(clicked()), this, SLOT(afficheFenetreOptionsFitts()));
+
+    setCentralWidget(fenetreStatistiquesFitts);
+
+}
+
+// Méthode appelée pour l'affichage de la boîte de dialogue "A propos"
+void FenetrePrincipale::aPropos() {
+
+    QMessageBox::about(this->parentWidget(), "A propos de nous", "Ce projet a été réalisé par Nicolas Hergenroether, Kieran Molina et Vincent Yucel dans le cadre de l'UV GL40 de l'UTBM.");
+
+}
